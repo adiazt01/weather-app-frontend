20316023108822220cdee19873b333146f8c0a3a
adiazt01 <adiaztovar2004@gmail.com>
2025-05-13 23:20:20 -0400

    feat: add server configuration to Vite for improved development setup

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ import { configDefaults } from 'vitest/config';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
